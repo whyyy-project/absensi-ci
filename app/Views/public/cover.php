b70f43ce4e2be82700fda30ce967d0ab67ed3ab3
Adimana12 <133749649+Adimana12@users.noreply.github.com>
2023-07-19 00:31:26 +0700

    update

--- app/Views/public/cover.php
+++ app/Views/public/cover.php
@@ -104,6 +104,7 @@
             </div>
         </header>
         <!-- end header section -->
+        
         <!-- slider section -->
         <section class="slider_section ">
             <div class="play_btn">
